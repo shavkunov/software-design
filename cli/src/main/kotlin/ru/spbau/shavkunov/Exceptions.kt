d56ce7155ea25263ecc4e3f047c1462d25069e21
shavkunov <mv.shavkunov@yandex.ru>
2018-03-29 20:01:33 +0300

    added comments to exceptions

--- cli/src/main/kotlin/ru/spbau/shavkunov/Exceptions.kt
+++ cli/src/main/kotlin/ru/spbau/shavkunov/Exceptions.kt
@@ -1,7 +1,16 @@
 package ru.spbau.shavkunov
 
+/**
+ * It's thrown, when tokenizer can't tokenize string
+ */
 class ParsingException : Exception()
 
+/**
+ * It's thrown, when grep command have problems with arguments
+ */
 class InvalidArgumentException: Exception()
 
+/**
+ * It's thrown when user provided empty command
+ */
 class EmptyCommandException : Exception()
